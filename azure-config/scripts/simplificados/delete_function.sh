b906592213b4765a372f301f3cc026eeea12d4a8
RianMacBook <rian@MacBook-Pro.local>
2021-04-12 20:50:49 -0300

    Ajustes scripts

--- azure-config/scripts/simplificados/delete_function.sh
+++ azure-config/scripts/simplificados/delete_function.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+function validation_message() {
+	echo -e "Arguments Requeried!\nEx: delete_fuction.sh <resourceGroup>  <planName>"
+	exit 1
+}
+
+function validation() {
+  local RESOURCE_GROUP=$1
+  local FUNCTION_APP_NAME=$2
+  local PREMIUM_PLAN_NAME=$3
+
+  if [ $# -lt 2 ]; then
+		validation_message
+		return 1
+	fi
+	return 0
+}
+
 function delete_functionapp() {
   local resourceGroup=$1
   local functionAppName=$2
@@ -30,24 +47,37 @@ function delete_function_app_insights() {
   return 0
 }
 
+
 # main
-RESOURCE_GROUP="XXXXXXXX"
-FUNCTION_APP_NAME=#<replace with function name>
+RESOURCE_GROUP=$1
+FUNCTION_APP_NAME='MyFunction198274'
+PREMIUM_PLAN_NAME=$2
+
+# Validation Input
+validation $RESOURCE_GROUP  $PREMIUM_PLAN_NAME
+if [ $? != 0 ]; then
+	validate_deploy_process 1 "End"
+	exit 1
+fi
 
 echo -e "Deleting Function $FUNCTION_APP_NAME...\n"
 delete_functionapp $RESOURCE_GROUP $FUNCTION_APP_NAME
 if [ $? != 0 ]; then
-		echo -e "\nFail to Delete Function App.\n"
+		validate_deploy_process 1 "Fail to Delete Function App."
 		exit 1
 else
-  echo -e "\nSuccess to Delete Function App.\n\n"
+  echo -e "Success to Delete Function App $FUNCTION_APP_NAME.\n\n"
 fi
+
 
 echo -e "\nDeleting Function App Insights $FUNCTION_APP_NAME...\n"
 delete_function_app_insights $RESOURCE_GROUP $FUNCTION_APP_NAME
 if [ $? != 0 ]; then
-		echo -e "\nFail to Delete Function App Insights.\n"
+		validate_deploy_process 1 "Fail to Delete Function App Insights."
 		exit 1
 else
-  echo -e "\nSuccess to Delete Function App Insights.\n"
+  echo -e "Success to Delete Function App Insights $FUNCTION_APP_NAME.\n"
 fi
+
+
+# func azure functionapp publish $functionAppName
