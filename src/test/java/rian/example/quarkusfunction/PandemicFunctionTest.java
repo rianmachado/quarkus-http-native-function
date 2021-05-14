d8ebb3759963d3fe7797793fda21de9bcc4caf2c
RianMacBook <rian@MacBook-Pro.local>
2021-05-13 22:37:47 -0300

    Remove Script

--- src/test/java/rian/example/quarkusfunction/PandemicFunctionTest.java
+++ src/test/java/rian/example/quarkusfunction/PandemicFunctionTest.java
@@ -5,12 +5,10 @@ import static org.hamcrest.CoreMatchers.containsString;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 
 @QuarkusTest
 public class PandemicFunctionTest {
@@ -23,20 +21,10 @@ public class PandemicFunctionTest {
 		args.put("yaer","2020");
 		RestAssured
 		.given()
-		.get("/ReportPandemicDemo?country=south-africa&month=12&year=2020")
+		.get("/ReportPandemic?country=south-africa&month=12&year=2020")
 		.then()
 		.statusCode(200)
 		.contentType("application/json").body(containsString(("2020-12")));
-	}
-	
-	@Test
-	public void reportPandemicPostTest() {
-		String body = "{\"otherInfo\":\"south-africa\"}";
-		Response response = RestAssured
-		.given()
-		.body(body)
-		.post("ReportPandemic");
-		Assertions.assertNotNull(response.path("guid"));
 	}
 
 }
